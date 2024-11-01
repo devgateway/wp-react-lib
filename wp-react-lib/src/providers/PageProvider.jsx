7ac3be3a890f9ca364f01750cb040ad3f5705648
Timothy Mugo <tmugo@developmentgateway.org>
2024-11-01 16:00:56 +0300

    DVIZ-10 add react compiler

--- wp-react-lib/src/providers/PageProvider.jsx
+++ wp-react-lib/src/providers/PageProvider.jsx
@@ -1,117 +1,86 @@
-import React from 'react'
-import {Container, Loader, Segment} from "semantic-ui-react";
-import {connect} from 'react-redux'
-import {PageContext} from './Context'
-import {clean, getPages} from "../reducers/actions";
+import React, { useEffect } from 'react'
+import { Container, Loader, Segment } from "semantic-ui-react"
+import { useDispatch, useSelector } from 'react-redux'
+import { PageContext } from './Context'
+import { clean, getPages } from "../reducers/actions"
 import LocalizedProvider from "./LocalizedProvider"
 
-/*
-Will load a post base ond passed properties and put in PostContext
-*/
+const PageProvider = (props) => {
+    const {
+        before,
+        perPage,
+        page,
+        fields,
+        parent,
+        slug,
+        store = "pages",
+        locale,
+        previewNonce,
+        previewId,
+        search,
+        noCache,
+        children,
+        fallbackComponent
+    } = props;
 
-class PageProvider extends React.Component {
+    const dispatch = useDispatch();
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        const {
+    const error = useSelector(state => state.getIn(['wordpress', store, 'error']));
+    const meta = useSelector(state => state.getIn(['wordpress', store, 'meta']));
+    const pages = useSelector(state => state.getIn(['wordpress', store, 'items']));
+    const loading = useSelector(state => state.getIn(['wordpress', store, 'loading']));
+
+    useEffect(() => {
+        dispatch(getPages({
             before,
             perPage,
             page,
             fields,
             parent,
             slug,
-            store = "pages",
+            store,
             locale,
             previewNonce,
             previewId,
             search,
             noCache
-        } = this.props
-        if (prevProps.parent !== parent || prevProps.slug !== slug || locale !== prevProps.locale || previewId !== prevProps.previewId | search != prevProps.search) {
-            this.props.onLoad({
-                before,
-                perPage,
-                page,
-                fields,
-                parent,
-                slug,
-                store,
-                locale,
-                previewNonce,
-                previewId,
-                search,
-                noCache
-            })
-        }
-    }
+        }));
 
-    componentDidMount() {
-        const {
-            before,
-            perPage,
-            page,
-            fields,
-            parent,
-            slug,
-            store = "pages",
-            locale,
-            previewNonce,
-            previewId,
-            search,
-            noCache
-        } = this.props
+        return () => {
+            dispatch(clean({ store }));
+        };
+    }, [parent, slug, locale, previewId, search]);
 
-        this.props.onLoad({before, perPage, page, fields, parent, slug, store, locale, previewNonce, previewId, search,noCache})
-    }
-
-
-    componentWillUnmount() {
-
-        const {before, perPage, page, fields, parent, slug, store = "pages", locale} = this.props
-        this.props.onClean({store})
-    }
-
-    render() {
-        const {pages, meta, loading, error, fallbackComponent, locale} = this.props
-        if (pages && pages.length > 0) {
-            return <PageContext.Provider value={{pages, meta, locale}}>{this.props.children}</PageContext.Provider>
-        } else if (error) {
-            return <Segment color={"red"}><h1>500</h1>
-                <p>The service is not available please try again in a few minutes</p></Segment>
-        } else if (loading) {
-            return (<Container>
-                <Loader inverted content='Loading'/>
-            </Container>)
-        } else if (loading === false) {
-            if (fallbackComponent) {
-                return <>{fallbackComponent}</>;
-            } else {
-                return <Container>
+    if (pages && pages.length > 0) {
+        return <PageContext.Provider value={{ pages, meta, locale }}>{children}</PageContext.Provider>;
+    } else if (error) {
+        return (
+            <Segment color={"red"}>
+                <h1>500</h1>
+                <p>The service is not available please try again in a few minutes</p>
+            </Segment>
+        );
+    } else if (loading) {
+        return (
+            <Container>
+                <Loader inverted content='Loading' />
+            </Container>
+        );
+    } else if (loading === false) {
+        if (fallbackComponent) {
+            return <>{fallbackComponent}</>;
+        } else {
+            return (
+                <Container>
                     <Segment color={"red"}>
                         <h1>404</h1>
                         <p>Can't find this page</p>
                     </Segment>
                 </Container>
-            }
+            );
         }
-        return null
     }
-}
-
-const mapStateToProps = (state, ownProps) => {
-
-    const {store = "pages"} = ownProps
-    return {
-        error: state.getIn(['wordpress', store, 'error']),
-        meta: state.getIn(['wordpress', store, 'meta']),
-        pages: state.getIn(['wordpress', store, 'items']),
-        loading: state.getIn(['wordpress', store, 'loading'])
-    }
-}
-
-
-const mapActionCreators = {
-    onClean: clean,
-    onLoad: getPages
+    return null;
 };
 
-export default connect(mapStateToProps, mapActionCreators)(LocalizedProvider(PageProvider));
+export default LocalizedProvider(PageProvider);
