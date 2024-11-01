6ccbb11e20e9fca39e797562dd16352a3604d4dd
Timothy Mugo <tmugo@developmentgateway.org>
2024-11-01 16:00:56 +0300

    DVIZ-10 add react compiler

--- wp-react-lib/src/providers/AppContextProvider.jsx
+++ wp-react-lib/src/providers/AppContextProvider.jsx
@@ -2,31 +2,18 @@ import React, { Suspense } from 'react'
 import { AppContext } from './Context';
 import { Dimmer, Loader } from 'semantic-ui-react';
 
-class AppContextProvider extends React.Component {
-
-
-    componentDidMount() {
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-    }
-
-    render() {
-        const { locale, store, getComponent } = this.props
-
-        return (
-            <AppContext.Provider value={{ store, getComponent, locale }}>
-                <Suspense fallback={
-                     <Dimmer active>
-                     <Loader>Loading</Loader>
-                 </Dimmer>
-                }>
-                    {this.props.children}
-                </Suspense>
-            </AppContext.Provider>
-        );
-    }
-
+const AppContextProvider = ({ locale, store, getComponent, children }) => {
+    return (
+        <AppContext.Provider value={{ store, getComponent, locale }}>
+            <Suspense fallback={
+                <Dimmer active>
+                    <Loader>Loading</Loader>
+                </Dimmer>
+            }>
+                {children}
+            </Suspense>
+        </AppContext.Provider>
+    );
 }
 
 export default AppContextProvider
